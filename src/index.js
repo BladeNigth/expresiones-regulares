b4e5cd6e3b68b36fa44d664306b2120a13f50712
LuisEGM <gamezluis777@gmail.com>
2020-10-20 02:37:45 -0500

    Dividiendo y organizando estilos con sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import './style/style.css';
+import './style/main.scss';
+
 ReactDOM.render(
     <App/> , document.getElementById("root")
 );
